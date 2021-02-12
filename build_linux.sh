d2436e3bd5abbdd4f193b20b0009439a01e494a2
Reinier Hernández Ávila <sasuke.reinier@gmail.com>
2021-02-11 20:15:58 -0500

    fix another silly error

--- build_linux.sh
+++ build_linux.sh
@@ -21,26 +21,26 @@ touch $FOLDER/DEBIAN/control
 touch $FOLDER/usr/share/applications/$PACKAGE_NAME.desktop
 
 cat >$FOLDER/DEBIAN/control <<EOL
-Package: {PACKAGE_NAME}
-Version: {VERSION}
+Package: ${PACKAGE_NAME}
+Version: ${VERSION}
 Section: python3
 Priority: extra
 Architecture: any
-Maintainer: {MAINTAINER} <{MAINTAINER_EMAIL}>
+Maintainer: ${MAINTAINER} <${MAINTAINER_EMAIL}>
 Description: Calculadora de los precios de los VPS de Etecsa
 EOL
 cat $FOLDER/DEBIAN/control
 
 cat >$FOLDER/usr/share/applications/$PACKAGE_NAME.desktop <<EOL
 [Desktop Entry]
-Version: {VERSION}
+Version: ${VERSION}
 Type: Application
-Name: {PACKAGE_NAME}
+Name: ${PACKAGE_NAME}
 GenericName: VPS Etecsa Calculator
 Comment: Calculadora de los precios de los VPS de Etecsa
-Exec=/usr/local/bin/{PACKAGE_NAME}
+Exec=/usr/local/bin/${PACKAGE_NAME}
 Terminal: false
-Keywords:VPS;Etecsa;Calculator;{PACKAGE_NAME}
+Keywords:VPS;Etecsa;Calculator;${PACKAGE_NAME}
 Categories=Productivity
 StartupNotify=true
 EOL
